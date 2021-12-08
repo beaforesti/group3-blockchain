89aa061b46820ceafbb066ceada61108b1a4be6d
beaforesti <bea_fiore@live.it>
2021-12-08 16:56:39 +0100

    new style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // JS file //
+function bigImg(x) {
+  x.style.height = "120px";
+  x.style.width = "120px";
+}
+
+function normalImg(x) {
+  x.style.height = "98px";
+  x.style.width = "98px";
+}
+
+function discoverMore(x) {
+  x.style.height = "120px";
+  x.style.width = "120px";
+}
+
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+});
